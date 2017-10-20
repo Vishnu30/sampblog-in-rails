7f01ec76838418e7f624c72d71c9542c78de467c
vishnubajpai_cybersafe <vishnu.bajpai@cybersafe.co.in>
2017-10-20 15:34:58 +0530

    source code

---
+++ app/helpers/application_helper.rb
@@ -0,0 +1,36 @@
+module ApplicationHelper
+
+	def get_season()
+    			time = Time.new
+    			if(time.month >= 3) && (time.month <= 5)
+      				"Yeah it is Spring"
+    			elsif (time.month > 5) && (time.month <= 8)
+      				"Everyone Loves Summer"
+    			elsif (time.month > 8) && (time.month <= 10)
+      				"Put on Your Coat because Fall is here"
+    			else
+      				"Yuck Winter"
+    			end
+    		end
+    def get_random_number(max_value = 10)
+    			rand(max_value)
+  			end
+
+  	def get_random_welcome()
+    		opening = ["What a beautiful day. ",
+                "Welcome to our site. ",
+                "Thank you for stopping by. "]
+
+    		middle = ["We hope you find what you need. ",
+                "We have a wide selection.  ",
+                "Check out our sale items. "]
+
+    		ending = ["Contact us if you need help. ",
+                "We are here to server you. ",
+                "Call us if you need to 412-555-1212. "]
+
+    		"#{opening[rand(3)]} #{middle[rand(3)]} #{ending[rand(3)]}"
+
+  			end
+  	
+end
